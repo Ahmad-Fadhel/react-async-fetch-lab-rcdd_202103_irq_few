56de6a9c2af95516064c78d430527c761f35c728
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-06-12 23:58:20 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 class App extends Component {
 
   state = {
-      astronautsInSpace: []
+      peopleInSpace: []
   }
 
   render() {
@@ -19,7 +19,7 @@ class App extends Component {
   componentDidMount() {
     fetch('http://api.open-notify.org/astros.json')
       .then(response => response.json())
-      .then(({people}) => this.setState({ astronautsInSpace: people }))
+      .then(({people}) => this.setState({ peopleInSpace: people }))
 
   }
 
